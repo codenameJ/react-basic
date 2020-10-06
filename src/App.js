5130700545b976403f7d555f609b8f76461037d2
BUNYAPORN BOONSEUB <s92120@scbcorp.co.th>
2020-10-06 09:33:01 +0700

    [update] app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import Person from "./Person/Person.js";
 
 function App() {
+
+  const state = useState({
+    persons: [
+            { name: "max", age: 11 },
+            { name: "xam", age: 17 },
+            { name: "axm", age: 24 },
+          ],
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, This is React</h1>
+      <Person name={state.persons[0].name} age={state.persons[0].age}/>
+      <Person name={state.persons[1].name} age={state.persons[1].age}>I'm the god</Person>
+      <Person name={state.persons[2].name} age={state.persons[2].age}/>
     </div>
   );
 }
+
+// class App extends React.Component {
+//   state = {
+//     persons: [
+//       { name: "max", age: 11 },
+//       { name: "xam", age: 17 },
+//       { name: "axm", age: 24 },
+//     ],
+//   };
+
+//   switchNameHandler = () => {
+//     // console.log("Switch");
+//     // this.state.persons[0].name = "SuperA"; //this is so f*cking sh*t react wont recognize this change dont do it!!!
+//     this.setState({
+//       persons: [
+//         { name: "SuperMax", age: 28 },
+//         { name: "SuperXam", age: 42 },
+//         { name: "axm", age: 24 },
+//       ],
+//     });
+//   };
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hi, This is React</h1>
+//         <button onClick={this.switchNameHandler}>Switch Name</button>
+//         <Person
+//           name={this.state.persons[0].name}
+//           age={this.state.persons[0].age}
+//         />
+//         <Person
+//           name={this.state.persons[1].name}
+//           age={this.state.persons[1].age}
+//         >
+//           I'm the god
+//         </Person>
+//         <Person
+//           name={this.state.persons[2].name}
+//           age={this.state.persons[2].age}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
