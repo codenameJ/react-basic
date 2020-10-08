66cd04fcf2b6893ff9151c0f126e0a39926bfd05
BUNYAPORN BOONSEUB <s92120@scbcorp.co.th>
2020-10-08 17:52:27 +0700

    [update] app css

--- src/App.js
+++ src/App.js
@@ -38,66 +38,92 @@ import Person from "./Person/Person.js";
 class App extends React.Component {
   state = {
     persons: [
-      { name: "max", age: 11 },
-      { name: "xam", age: 17 },
-      { name: "axm", age: 24 },
+      { id: "1", name: "max", age: 11 },
+      { id: "2", name: "xam", age: 17 },
+      { id: "3", name: "axm", age: 24 },
     ],
   };
 
-  switchNameHandler = (NewName) => {
-    // console.log("Switch");
-    // this.state.persons[0].name = "SuperA"; //this is so f*cking sh*t react wont recognize this change dont do it!!!
+  // console.log("Switch");
+  // this.state.persons[0].name = "SuperA"; //this is so f*cking sh*t react wont recognize this change dont do it!!!
+
+  deletePersonHandler = (index) => {
+    // const persons = this.state.persons;
+    const persons = [...this.state.persons]; // new javaScript ES6
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
+  };
+  nameChangeHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+    const person = { ...this.state.persons[personIndex] };
+    // const person = Object.assign({}, this.state.persons[personIndex]); // Old version to assign object to arr
+
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: NewName, age: 28 },
-        { name: "SuperXam", age: 42 },
-        { name: "axm", age: 24 },
-      ],
+      persons: persons,
     });
   };
 
-  nameChangeHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 42 },
-        { name: "axm", age: 24 },
-      ],
-    });
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   };
+
   render() {
     const style = {
-      backgroundColor: "white",
+      backgroundColor: "green",
+      color: "white",
       font: "inherit",
       border: "1px solid blue",
       padding: "8px",
-      cursor: 'pointer'
+      cursor: "pointer",
     };
+
+    let persons = null;
+
+    if (this.state.showPersons === true) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={(event) => this.nameChangeHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+      style.backgroundColor = "red"
+    }
+
+    // let classes = ['red', 'bold'].join(' '); // assign by join with white space
+    // let classes = "red bold" // result is same as previous line
+
+    let classes = [];
+    if(this.state.persons.length <=2){
+      classes.push('red'); // classes = ['red']
+    }
+    if(this.state.persons.length <=1){
+      classes.push('bold'); // classes = ['red', 'bold']
+    }
 
     return (
       <div className="App">
         <h1>Hi, This is React</h1>
-        <button 
-        style={style}
-        onClick={() => this.switchNameHandler("GodMax!!")}>
-          Switch Name
+        <p className={classes.join(' ')}>hey, you see?</p>
+        <button style={style} onClick={this.togglePersonsHandler}>
+          Switch
         </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          click={this.switchNameHandler.bind(this, "JuniorMax")}
-          changed={this.nameChangeHandler}
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          I'm the god
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+        {persons}
       </div>
     );
   }
